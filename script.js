92c8dfd0d934f29fd11b3059d8e0c7995d6aa7ea
Hussein Nasser <40250396+IQSP3D@users.noreply.github.com>
2025-01-14 17:34:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+let btn = document.querySelectorAll(".show-modal");
+let windowInformaion = document.querySelector(".modal");
+let overlay = document.querySelector(".overlay");
+let closebutton = document.querySelector(".close-modal");
+function toggleOpenCloseWindow() {
+  if (windowInformaion.classList.contains("hidden")) {
+    overlay.classList.remove("hidden");
+    return windowInformaion.classList.remove("hidden");
+  }
+  overlay.classList.add("hidden");
+  return windowInformaion.classList.add("hidden");
+}
+closebutton.addEventListener("click", toggleOpenCloseWindow);
+btn.forEach((element) => {
+  element.addEventListener("click", toggleOpenCloseWindow);
+});
